17ec377ca7ce45587c6fe581a2490bb479d934d8
veta306 <vv097099@gmail.com>
2024-06-16 13:02:00 +0300

    home page and cards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,14 @@ import arrowDown from '../../assets/images/arrow_down.png';
 import styles from './home.module.css';
 import cardImage from '../../assets/images/card_image_example.png';
 import partyLogo from '../../assets/images/party_logo_example.png';
+import smallCardImage from '../../assets/images/small_card_image_example.png';
 import Footer from '../../components/footer';
+import { Icon } from '@iconify/react';
+import PersonCard from '../../components/personCard';
+import { PersonCardInfo, ShameCardInfo } from '../../types';
+import ShameCard from '../../components/shameCard';
 
-const ratingCards = [
+const ratingCards: PersonCardInfo[] = [
   {
     image: cardImage,
     count: 59,
@@ -27,6 +32,53 @@ const ratingCards = [
     name: 'ОЛЕНА ПЕТРЕНКО',
     party: 'Голос України',
     logo: partyLogo,
+  },
+];
+
+const latestCards: ShameCardInfo[] = [
+  {
+    image: smallCardImage,
+    name: 'МАКСИМ ШЕВЧЕНКО',
+    add: '+2',
+    date: '22 БЕР 2024',
+    description: 'Депутат викритий на хабарі: час для справедливості!',
+  },
+  {
+    image: smallCardImage,
+    name: 'ОЛЕНА ПЕТРЕНКО',
+    add: '+4',
+    date: '20 БЕР 2024',
+    description: "Політик фальсифікував вибори: громадськість має об'єднатися!",
+  },
+  {
+    image: smallCardImage,
+    name: 'ОЛЕГ СИДОРЕНКО',
+    add: '+3',
+    date: '15 БЕР 2024',
+    description:
+      'Корупція у владних коридорах сягнула жахливих масштабів: потрібні рішучі дії!',
+  },
+  {
+    image: smallCardImage,
+    name: 'МАКСИМ ШЕВЧЕНКО',
+    add: '+5',
+    date: '09 БЕР 2024',
+    description:
+      'Незаконне збагачення політиків - це злочин проти народу: час покласти край!',
+  },
+  {
+    image: smallCardImage,
+    name: 'МАКСИМ ШЕВЧЕНКО',
+    add: '+3',
+    date: '03 БЕР 2024',
+    description: 'Корупція у владних коридорах: потрібні рішучі дії!',
+  },
+  {
+    image: smallCardImage,
+    name: 'МАКСИМ ШЕВЧЕНКО',
+    add: '',
+    date: '01 БЕР 2024',
+    description: 'Міністр зловживав владою: не можна залишати безкарним!',
   },
 ];
 
@@ -63,36 +115,83 @@ const HomePage: FC = () => {
             </div>
             <div className={styles.ratingButtons}>
               <button className={styles.ratingButton}>
-                РЕЙТИНГ ЗАШКВАРІВ<span className={styles.arrowRight}></span>
+                РЕЙТИНГ ЗАШКВАРІВ
+                <Icon
+                  icon="fontisto:arrow-right"
+                  className={styles.arrowRight}
+                ></Icon>
               </button>
             </div>
           </div>
           <div className={styles.ratingCards}>
-            {ratingCards.map((ratingCard, index) => {
-              return (
-                <div key={index} className={styles.ratingCard}>
-                  <img
-                    src={ratingCard.image}
-                    alt="card image"
-                    className={styles.ratingCardImage}
-                  />
-                  <p className={styles.ratingCardCount}>
-                    {ratingCard.count} ЗАШКВАРІВ
-                  </p>
-                  <p className={styles.ratingCardName}>{ratingCard.name}</p>
-                  <div className={styles.ratingCardParty}>
-                    <img
-                      src={ratingCard.logo}
-                      alt="party logo"
-                      className={styles.partyLogo}
-                    />
-                    <p className={styles.partyName}>{ratingCard.party}</p>
-                  </div>
-                </div>
-              );
-            })}
+            {ratingCards.map((ratingCard, index) => (
+              <PersonCard key={index} {...ratingCard} />
+            ))}
           </div>
           <div className={styles.ratingLine}></div>
+          <div className={styles.slider}>
+            <div className={styles.sliderButton}>
+              <Icon icon="bxs:chevron-left" className={styles.sliderIcon} />
+            </div>
+            <div className={styles.sliderButton}>
+              <Icon icon="bxs:chevron-right" className={styles.sliderIcon} />
+            </div>
+          </div>
+        </section>
+        <section className={styles.remember}>
+          <div className={styles.rememberInfo}>
+            <p className={styles.rememberHead}>
+              ПАМ’ЯТАЙ ПРО УСІ ЗАШКВАРИ ДЕПУТАТІВ ТА ЧИНОВНИКІВ
+            </p>
+            <p className={styles.rememberText}>
+              Наша мета - інформувати громадськість про зашквари депутатів{' '}
+              <br /> та чиновників, засудити їхню поведінку та сприяти їхньому
+              покаранню.
+            </p>
+          </div>
+          <div className={styles.rememberLinks}>
+            <div className={styles.rememberLink}>
+              <p className={styles.linkText}>ЗАШКВАРИ</p>
+              <Icon
+                icon="fontisto:arrow-right"
+                className={styles.linkIcon}
+              ></Icon>
+            </div>
+            <div className={styles.thinLine} />
+            <div className={styles.rememberLink}>
+              <p className={styles.linkText}>ОСОБИ</p>
+              <Icon
+                icon="fontisto:arrow-right"
+                className={styles.linkIcon}
+              ></Icon>
+            </div>
+          </div>
+        </section>
+        <section className={styles.latest}>
+          <div className={styles.latestHeader}>
+            <div className={styles.latestInfo}>
+              <p className={styles.latestTitle}>ОСТАННІ ЗАШКВАРИ</p>
+              <p className={styles.latestDescription}>
+                Оновлюваний перелік епізодів корупції, хабарництва, зловживання
+                владою, некомпетентності та інших неприйнятних вчинків
+                представників влади.
+              </p>
+            </div>
+            <div className={styles.latestButtons}>
+              <button className={styles.latestButton}>
+                ВСІ ЗАШКВАРИ
+                <Icon
+                  icon="fontisto:arrow-right"
+                  className={styles.arrowRight}
+                ></Icon>
+              </button>
+            </div>
+          </div>
+          <div className={styles.latestCards}>
+            {latestCards.map((latestCard, index) => (
+              <ShameCard key={index} {...latestCard} />
+            ))}
+          </div>
         </section>
       </main>
       <Footer />
